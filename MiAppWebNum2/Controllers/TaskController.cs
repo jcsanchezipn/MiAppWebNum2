d0fa2ba734ab0e3cd7105ec2e32d3de4950d9e99
JuanSanchez <jcsanchez@ipntijuana.mx>
2020-11-25 16:58:53 -0800

    Version 1

---
+++ MiAppWebNum2/Controllers/TaskController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiAppWebNum2.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Ciclos()
+        {
+            return View();
+        }
+    }
+}
